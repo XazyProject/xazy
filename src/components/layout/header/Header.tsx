9b8ecc8dae33866c5161de075f8e1a69ae0ee116
Luka Pitskhelauri <luka.pitsk@gmail.com>
2024-01-10 01:05:52 +0400

    add toggleDarkMode with localstorage

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,12 +1,17 @@
-
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../../../redux/store";
+import { toggleDarkMode } from "../../../redux/darkModeSlice";
 
 const Header = () => {
+    const { mode } = useSelector((state: RootState) => state.darkMode)
+    const dispatch = useDispatch();
+    console.log(mode)
     return (
         <div>
             <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem ex nostrum mollitia. A magnam unde, porro commodi deserunt delectus ducimus iusto nulla cum eius animi non dolor ea perferendis minima.
             </p>
-            <button>
+            <button onClick={() => dispatch(toggleDarkMode())}>
                 toggle
             </button>
         </div>
